b1b8796be65e0cd519698cd983ff87c8b75a4436
Fabiha Ahmed <fsafabiha@gmail.com>
2020-03-04 19:06:53 -0500

    initial files

---
+++ Ahmed-Gomes-Jain-Yang-decrypt.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+    cout << "Enter the ciphertext:" << endl;
+    string s;
+    cin >> s;
+    cout << "My plaintext guess is:" << endl;
+    //code stuff
+}
