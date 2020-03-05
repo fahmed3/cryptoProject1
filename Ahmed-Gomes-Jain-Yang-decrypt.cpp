ca964a5a103f5ae7fb47f9e7e233bc1f0fc40ce1
Tanya Jain <tanyajain0018@gmail.com>
2020-03-05 01:31:09 -0500

    Update Ahmed-Gomes-Jain-Yang-decrypt.cpp

--- Ahmed-Gomes-Jain-Yang-decrypt.cpp
+++ Ahmed-Gomes-Jain-Yang-decrypt.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 #include <stdlib.h>
+#include <map>
+#include<string>
+#include <iostream>
+#include <fstream>
+
 using namespace std;
 
-int main(){
-    cout << "Enter the ciphertext:" << endl;
-    string s;
-    cin >> s;
-    cout << "My plaintext guess is:" << endl;
-    //code stuff
+int main() {
+	cout << "Enter the ciphertext:" << endl;
+	string s, word;
+	getline(cin, s);
+	cout << "My plaintext guess is:" << endl;
+
+	map<char, int >encrypted;
+	map<char, int >::iterator itr;
+	for (int i = 0; i < s.length(); i++)
+		encrypted[s[i]]++;
+
+	// The for loop below is for testing purposes only to see if it counts all letters correctly
+	for (itr = encrypted.begin(); itr != encrypted.end(); itr++)
+		cout << itr->first << " : " << itr->second << "; ";
+
+	ifstream ifs("plaintext_dictionary_test1.txt");
+	if (!ifs) {
+		cerr << "Could not open the file.\n";
+		exit(1);
+	}
+
+	while (ifs >> word) {
+		//cout << word << " ";
+		if (word == "1") {
+			map<char, int >text1;
+			
+		}
+
+	}
 }
