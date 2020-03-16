797100b1b1e043ff261e10bd27cbf77c361be240
avik-gomes02 <47250706+avik-gomes02@users.noreply.github.com>
2020-03-15 21:43:58 -0400

    Update Ahmed-Gomes-Jain-Yang-decrypt.cpp
    FINAL CODE

--- Ahmed-Gomes-Jain-Yang-decrypt.cpp
+++ Ahmed-Gomes-Jain-Yang-decrypt.cpp
@@ -5,15 +5,16 @@
 #include <fstream>
 using namespace std;
 
-void readFileFromStream(ifstream& stream, vector<int>& text1, vector<int>& text2, vector<int>& text3, 
-						vector<int>& text4, vector<int>& text5);
+//prototypes
+void readFileFromStream(ifstream& stream, vector<int>& text1, vector<int>& text2, vector<int>& text3,
+	vector<int>& text4, vector<int>& text5);
 vector<int> diff(vector<int>& ciphertext, vector<int>& plaintext);
 bool ptnCheck(vector<int>& difference);
 vector<int> keyExists(vector<int>& difference);
 void encode(vector<int>& v, string word);
 void diffCombos(const string str[], string prefix, const int n, const int len, vector<vector<int>>& ret);
 string decrypt(vector<int>& cipher, vector<int>& shifts, string words[]);
-bool correctCipher(string& answer, string str[]);
+bool correctPlainText(string& answer, string str[]);
 
 int main() {
 	cout << "Enter Ciphertext" << endl;
@@ -26,25 +27,24 @@ int main() {
 	vector<int> cipherText;
 	vector<int> asciiDiff;
 
-	//getting the ascii value of the char of the string @ i
+	//pushing the ascii values of the inputed ciphertext into the vector cipherText
 	for (size_t i = 0; i < s.size(); i++) {
 		cipherText.push_back(int(s[i]));
-		//Since the Ascii Value of ' ' is different than what we wanted
-		//Each time there was a ' ' we just make it a 96
 		if (s[i] == ' ') {
 			cipherText.pop_back();
 			cipherText.push_back(96);
 		}
 	}
 
-	//puting the first 49 characters into a vector
-	//because idk why we gotta use a map if were not counting occurances
+	//for test 1
 	ifstream ifs("plaintext_dictionary_test1.txt");
 	if (!ifs) {
 		cerr << "Could not open the file.\n";
 		exit(1);
 	}
 
+	//separating test1 plaintext into 5 different vectors
+	//represented by their ASCII values
 	readFileFromStream(ifs, text1, text2, text3, text4, text5);
 	vectorPlaintexts.push_back(text1);
 	vectorPlaintexts.push_back(text2);
@@ -52,12 +52,14 @@ int main() {
 	vectorPlaintexts.push_back(text4);
 	vectorPlaintexts.push_back(text5);
 
+	//Loop used to see if the ciphertext was the ciphertext for
+	//test one. if it is, it then attemps to output what ciphertext
+	//it is
 	int crtPlt;
-	//function to find the difference
 	for (int i = 0; i < 5; i++) {
+		//gets the ASCII difference between each plaintext and the ciphertext
 		asciiDiff = diff(cipherText, vectorPlaintexts[i]);
-
-		if (ptnCheck(asciiDiff)) {
+		if (ptnCheck(asciiDiff)) { //if a pattern exists its Test One
 			crtPlt = i;
 			ifstream ifs("plaintext_dictionary_test1.txt");
 			if (!ifs) {
@@ -84,17 +86,17 @@ int main() {
 	//known words in the second dictionary, with a space at the end,
 	//so they were hardcoded to save time. Ordered from the longest to smallest in 
 	//character count
-	string str[] = {"unconvertible ","intuitiveness ","hermeneutics ","photocompose ", 
-					"miserabilia ","faultlessly ","awesomeness ","attentional ", 
-					"dismissive ","delimiting ","racecourse " ,"directions ", 
-					"indelicacy ","shorelines " ,"combusting " ,"successors ", 
+	string str[] = { "unconvertible ","intuitiveness ","hermeneutics ","photocompose ",
+					"miserabilia ","faultlessly ","awesomeness ","attentional ",
+					"dismissive ","delimiting ","racecourse " ,"directions ",
+					"indelicacy ","shorelines " ,"combusting " ,"successors ",
 					"postilion ","footfalls ","pintsized ","courtship ","aloneness ",
 					"hearkened ","protruded ","proposes ","memphis ","rustics ",
-					"between ","repress ","matures ","pressed ","ferries ", 
+					"between ","repress ","matures ","pressed ","ferries ",
 					"catcher ","bursary ","beheld ","swoops ","chuted ","wobbly " ,
-					"myrtle ","cadgy ", "irony "};
+					"myrtle ","cadgy ", "irony " };
 
-	int len = 3,n = 40; // 3 words per permuatation out of all the 40 words in the
+	int len = 3, n = 40; // 3 words per permuatation out of all the 40 words in the
 						// second dictionary 
 
 	vector<vector<int>> possCipher;
@@ -104,6 +106,8 @@ int main() {
 	string a = "";
 	bool found;
 	for (size_t index = 0; index < possCipher.size(); index++) {
+		//if the possible cipher has a size of 24 we can compare
+		//and decrypt because the size of the key happens at least once
 		if (possCipher[index].size() >= 24) {
 			asciiDiff = diff(cipherText, possCipher[index]);
 			//Need to shorten posscipher[index], and possibly ciphertext
@@ -111,21 +115,23 @@ int main() {
 			vector<int> temp = keyExists(asciiDiff);
 			if (temp.size() != 0) {
 				answer = decrypt(cipherText, temp, str);
-				if (correctCipher(answer, str)) {
+				if (correctPlainText(answer, str)) {
 					cout << answer << endl;
 					exit(1);
 				}
 			}
 		}
-		else{
-			for(int i = 0; i < 40; i++){
+		//if the size of the possible cipher is less than 24, one more word is inputed
+		//to increase the chances that the full key exists in the subset of characters
+		else {
+			for (int i = 0; i < 40; i++) {
 				vector<int> newWord = possCipher[index];
-				encode(newWord,str[i]);
+				encode(newWord, str[i]);
 				asciiDiff = diff(cipherText, newWord);
 				vector<int> temp = keyExists(asciiDiff);
 				if (temp.size() != 0) {
 					answer = decrypt(cipherText, temp, str);
-					if (correctCipher(answer, str)) {
+					if (correctPlainText(answer, str)) {
 						cout << answer << endl;
 						exit(1);
 					}
@@ -133,12 +139,14 @@ int main() {
 			}
 		}
 	}
+	//output if everything else fails
 	cout << "PlainText Not found. Sorry Professor :(\n";
 	return 0;
 }
 
+//function used to read from the file and place into a specific vector representing each plaintext in test 1
 void readFileFromStream(ifstream& stream, vector<int>& text1, vector<int>& text2, vector<int>& text3, vector<int>& text4, vector<int>& text5) {
-	void inputWordsFromStream(ifstream & ifs, vector<int>& push, char& moreWords, char& word);
+	void inputWordsFromStream(ifstream & ifs, vector<int> & push, char& moreWords, char& word);
 	char word, moreWords;
 	string w;
 	string plt;
@@ -163,9 +171,9 @@ void readFileFromStream(ifstream& stream, vector<int>& text1, vector<int>& text2
 	}
 
 }
-
+//used as a helper function for the readFileFromStream, to help input the words into a vector
 void inputWordsFromStream(ifstream& ifs, vector<int>& push, char& moreWords, char& word) {
-	ifs>> word;
+	ifs >> word;
 	ifs.get(moreWords);
 	ifs.get(moreWords);
 	for (int i = 0; i < 48; i++) {
@@ -192,7 +200,12 @@ vector<int> diff(vector<int>& ciphertext, vector<int>& plaintext) {
 	return asciiDiff2;
 }
 
-
+//input: vector<int> that is all the shifts we found from diff
+//loops through the shifts to check if there is a pattern
+//if one does not begin within the first 24 ints,
+// we return false since a pattern longer than 24 is not possible with a max key length of 24
+//we return if we spot a pattern and it continues until the 48th character
+// if it does, we confirmed our cycle
 bool ptnCheck(vector<int>& difference) {
 	int curr = 0;
 	bool inCycle = false;
@@ -256,6 +269,7 @@ vector<int> keyExists(vector<int>& difference) {
 }
 
 //takes in vector<int> (v[index]) and a word to push into the vector<int>
+//converts string word into it's ascii value and pushes back into vector
 void encode(vector<int>& v, string word) {
 	int num_form;
 	for (size_t i = 0; i < word.size(); i++) {
@@ -268,20 +282,21 @@ void encode(vector<int>& v, string word) {
 }
 
 
-// The main recursive method to print all possible strings of length "len"
+// The main recursive method to print all possible strings of length 3
 void diffCombos(const string str[], string prefix, const int n, const int len, vector<vector<int>>& ret) {
 	if (len == 1) {
 		for (int j = 0; j < n; j++) {
-			//cout << prefix + str[j] << endl;
 			string x = prefix + str[j];
 			vector<int> v;
+			//encodes the permutation to it's ASCII values and then
+			//places it in the return (ret) vector
 			encode(v, x);
 			ret.push_back(v);
 		}
-	}//Base case: len = 1, print the string "leng" times + the remaining letter
+	}
 	else {
-		// One by one add all characters from "str" and recursively call for "lenght" equals to "lenght"-1
 		for (int i = 0; i < n; i++)
+			//recursive call to get all possible combinations of the string
 			diffCombos(str, prefix + str[i], n, len - 1, ret);
 	}
 
@@ -291,11 +306,17 @@ void diffCombos(const string str[], string prefix, const int n, const int len, v
 string decrypt(vector<int>& cipher, vector<int>& shifts, string words[]) {
 	string holder = "";
 	for (int index = 0; index < cipher.size(); index++) {
+		//we are shifting it back, and when this is done sometimes it goes
+		//past the character 'a' by 27 so the condition is needed to make sure the
+		//proper shift can happen if it goes past 'a'
 		if (cipher[index] - shifts[index % shifts.size()] < 97) {
+			//the ' ' character appearead as the '}' so whenever the '{' character appears
+			//when decrypting it is replaced with a ' '
 			if ((char(cipher[index] + (27 - shifts[index % shifts.size()]))) == '{') {
 				holder += " ";
 			}
 			else
+				//decrypting a regular character
 				holder += char(cipher[index] + (27 - shifts[index % shifts.size()]));
 		}
 		else {
@@ -309,13 +330,15 @@ string decrypt(vector<int>& cipher, vector<int>& shifts, string words[]) {
 	return holder;
 }
 
-//answer - possible correct strings ,
-bool correctCipher(string& answer, string str[]) {
+//answer - possible correct strings , str - array of 40 words
+//loops through potential plaintext answer word by word
+//checks if each word exists in our words array
+bool correctPlainText(string& answer, string str[]) {
 	bool found = true;
 	string word = "";
 	//we loop through words of answer
-	for(size_t i = 0; i < answer.size(); i++){
-		if(answer[i] == ' '){
+	for (size_t i = 0; i < answer.size(); i++) {
+		if (answer[i] == ' ') {
 			word += answer[i];
 			if (find(str, str + 40, word) == (str + 40)) {
 				found = false;
